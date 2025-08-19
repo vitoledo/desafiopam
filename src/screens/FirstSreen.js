63935c660582d2c9519ddc3aff77266dbe715705
vitoledo <vitoledors@gmail.com>
2025-08-18 21:32:22 -0300

    finalizando firstscreen

--- src/screens/FirstSreen.js
+++ src/screens/FirstSreen.js
@@ -6,15 +6,23 @@ export default function FirstScreen({ navigation }) {
         <View style={styles.container}>
             <Image
                 source={require('../images/casual_dog.png')}
-                style={styles.img}
+                style={styles.imgdog}
             />
             <Text style={styles.title}>Ótimo dia!</Text>
             <Text style={styles.subtitle}>Como deseja acessar?</Text>
+
             <TouchableOpacity style={styles.buttonaces}>
-                <Text style={styles.buttonText}>Como deseja acessar?</Text>
+                <View style={styles.viewaces}>
+                    <Image
+                        source={require('../images/Google.png')}
+                        style={styles.imggoogle}
+                    />
+                    <Text style={styles.buttontxt1}>Como deseja acessar?</Text>
+                </View>
             </TouchableOpacity>
+
             <TouchableOpacity style={styles.buttonopc} onPress={() => navigation.navigate('First')}>
-                <Text style={styles.buttonText}>Outras opções</Text>
+                <Text style={styles.buttontxt2}>Outras opções</Text>
             </TouchableOpacity>
         </View>
     );
@@ -27,25 +35,59 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    img: {
-        width: 400,
-        height: 400,
+    imgdog: {
         marginBottom: 20,
     },
+    imggoogle: {
+        width: 25,
+        height: 25,
+    },
+    viewaces: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: '100%',
+        position: 'relative',
+        paddingLeft: 15,
+    },
     title: {
-        fontSize: 24,
+        fontSize: 30,
         fontWeight: 'bold',
         marginBottom: 10,
     },
     subtitle: {
-        fontSize: 15,
+        fontSize: 13,
         color: '#888',
         marginBottom: 20,
     },
     buttonaces: {
-        backgroundColor: '#007bff'
+        backgroundColor: '#14C871',
+        borderRadius: 5,
+        width: 350,
+        height: 50,
+        marginBottom: 20,
     },
     buttonopc: {
-
+        backgroundColor: '#ffffff',
+        borderRadius: 5,
+        borderWidth: 1.5,
+        borderColor: '#14C871',
+        width: 350,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttontxt1: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 15,
+        color: 'white',
+    },
+    buttontxt2: {
+        fontWeight: 'bold',
+        fontSize: 15,
+        color: 'black',
     },
 });
